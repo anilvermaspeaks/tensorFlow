ed1c912b13a6d3638716f8dad7c8fca873de063d
anil verma <anilcs240@gmail.com>
2020-10-17 13:32:31 +0530

    added img upload

--- image-classifier/sketch.js
+++ image-classifier/sketch.js
@@ -1,9 +1,19 @@
 let classifier;
 let item
-function setup() {
+const loader = document.getElementById('loader');
+const uploadField = document.querySelector('.image-upload');
+function setup(path) {
+loader.style.display='inline-block'
   const myCanvas = createCanvas(400, 400);
   myCanvas.parent("sketch-container");
-  item = createImg('./fish.jpg', loadImgOnReady);
+  let src = ''
+  if (path) {
+    src = `${path}`
+  }
+  else {
+    src = `./fish.jpg`
+  }
+  item = createImg(`${src}`, loadImgOnReady);
   item.hide()
   background(0);
   classifier = ml5.imageClassifier('MobileNet', predictLabel)
@@ -20,11 +30,51 @@ function predictLabel() {
 // show label and probability
 function showLabel(error, data) {
   let label = 'Try Again!!!'
-  let conf= ''
+  let conf = 0
   if (!error) {
     label = data[0].label;
     conf = data[0].confidence;
   }
   document.getElementById('imageLabel').textContent = label;
   document.getElementById('conf').textContent = `Probability ${conf}`
+  loader.style.display='none'
 }
+
+
+
+function initImageUpload() {
+  loader.style.display='none'
+  uploadField.addEventListener('change', getFile);
+
+  function getFile(e) {
+    let file = e.currentTarget.files[0];
+    checkType(file);
+  }
+
+  function previewImage(file) {
+    reader = new FileReader();
+    reader.onload = function () {
+      setup(reader.result)
+    }
+    reader.readAsDataURL(file);
+
+  }
+
+  function checkType(file) {
+    let imageType = /image.*/;
+    if (!file.type.match(imageType)) {
+      alert('Please upload valid image')
+      throw 'Please upload valid image';
+    } else if (!file) {
+      alert('Please upload image')
+      throw 'Please upload image';
+    } else {
+      previewImage(file);
+    }
+  }
+
+}
+
+initImageUpload();
+
+
